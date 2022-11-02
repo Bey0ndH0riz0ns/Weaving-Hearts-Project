996e4a8961e45d86af7c856243fa327d993daee1
Beyond Horizons <beyond.the.viewed.horizons@gmail.com>
2022-11-02 20:40:19 +0200

    Update

--- codeStoryBuilder.py
+++ codeStoryBuilder.py
@@ -18,103 +18,53 @@ row = 1
 for data in scpFile.readlines():
     line = data.rstrip()
     if sceneBeginning.match(line):
-        #print(line)
         Index = re.findall('\d', line)[0]
         Scene = True
         continue
     if sceneEnd.match(line):
-        #print(line)
         SceneMap[str(Index)] = sceneLines
         Scene = False
         sceneLines = []
         row = 1
         continue
     if (Scene) and (len(line) > 1):
-        #print(line)
         sceneLines.append({'row': row, 'line':line})
         row = row + 1
-        #print('')
-        #print(line)
-        #if not str(Index) in SceneMap:
-        #    SceneMap[str(Index)] = [line]
-        #    continue
         
-#print('')
-#print(SceneMap['1'])
-
-#print(len(SceneMap['1']))
-
-#for data in SceneMap['1']:
-#    print(data)
-#    print('')
-
-#for data in SceneMap['2']:
-#    print(data)
-#    print('')
-
-#for data in SceneMap['3']:
-#    print(data)
-#    print('')
-
-#print(Script)
-
 beginningPattern = re.compile("    \"{i}([A-Za-z]+, [A-Za-z]+ [0-9]\*){\/i}\"")
 endPattern = re.compile("    \"{i}Scene End{\/i}\"")
 dialogPattern = re.compile("(    ([a-z])+ +\".+\")|(    \"[A-Za-z ]+\" \".+\")|(    \"\.\.\.\")|(    \"\(.+\)\")") # Problems here
 commentPattern = re.compile("    #.+")
 settingPattern = re.compile("    \"{i}[A-Za-z? ]+{\/i}\"")
 
-
 sceneNumber = '0'
 Scene = False
 row = 0
-for line in rpyFile.readlines():
+for data in rpyFile.readlines():
+    line = data.rstrip()
+    
     if beginningPattern.match(line):
-        #print(line)
-        #print('')
         sceneNumber = re.findall('\d', line)[0]
-        #print(sceneNumber)
         Scene = True 
+    
     if endPattern.match(line):
-        #print(line)
-        #print('')
         row = 0
         Scene = False
+    
     if (dialogPattern.match(line) or commentPattern.match(line) or settingPattern.match(line)) and Scene and sceneNumber in SceneMap:
-        print(row+1)
-        print(line)
-        #print(sceneNumber)
-        print(SceneMap[sceneNumber][row])
-        print('')
+        #temp = re.sub('({[a-z]=[0-9.]+})|({i})|({\/i})', '',line.strip())
+        temp = re.sub('({[a-z]+=[*0-9.]+})|({[a-z]+})|({\/[a-z]+})', '',line.strip())
+        lineWords = re.findall('\w+',temp)
+        sceneWords = re.findall('\w+',SceneMap[sceneNumber][row]['line'])
         
+        if len(lineWords) == len(sceneWords):
+            print(lineWords)
+            print(sceneWords)
+            print('')
         
-        #for data in SceneMap[sceneNumber]:
-            #if data['row'] == row:
-                #print(row)
-                #print(line)
-                #print(data)
-            
-            #print(data)
-            #print('')
-        
-        #for sceneLine in SceneMap[sceneNumber]:
-        #    if sceneLine['row'] == row:
-        #        print(line)
-        #        print(sceneLine['line'])
-        #        print('')
+        #print(re.findall('\w+',temp))
+        #print(re.findall('\w+',SceneMap[sceneNumber][row]['line']))
+        #print('')
         row = row + 1
-       #for sceneLine in SceneMap[sceneNumber]:
-            #lintedFile.write(sceneLine)
-            #temp1 = line.split()
-            #temp2 = sceneLine.split()
-            #if temp1 in temp2:
-            #    print(temp1)
-            #    print(temp2)
-            #    print('')
-            #if line in sceneLine:
-            #    print(sceneLine)
-            #    print('')
-        #    print(sceneLine) 
-        #    print('')    
-       #print(line)
+        
     lintedFile.write(line)
